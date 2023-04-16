40cb65124110bf4f1d03593a40092c5685f9093a
azkaban <pramusintokh@gmail.com>
2023-04-16 13:38:12 +0700

    third commit

--- bspline - aic.R
+++ bspline - aic.R
@@ -1,5 +1,4 @@
 library(splines)
-<<<<<<< HEAD
 MPL<-function(x,eps=1e-009)
 {
   x<-as.matrix(x)
@@ -26,19 +25,13 @@ y<-c(-0.05,0.15,0.10,0.08,0.46,0.56,-0.26,-0.11,0.13,0.46,0.57,0.42,-0.08,0.26,0
 plot(x, type = "l")
 
 # ynaive = x
-=======
-x<-c(1.25,1.01,0.56,0.21,1.08,2.51,1.31,0.67,1.15,0.62,0.07,-0.11,0.09,0.32,0.18,-0.34,0.27,0.18,0.32,0.77,0.8,-0.03,0.09,0.24,0.57,0.31,0.13,0.25,0.14,1.26,1.4,0.43,1.06,0.28,0.62,0.72,0.84,0.1,0.21,-0.28,0.13,0.26,0.9,0.63,0.19,0.04,0.33,0.48,0.25,0.1,0.36,0.11,0.05,0.75,0.76,0.42,0.19,0.38,0.2,0.66,0.96,0.93,0.79,-0.3,-0.29,0.84,2.58,0.87,-0.24,0.61,0.2,0.17)
-y<-c(1.01,0.56,0.21,1.08,2.51,1.31,0.67,1.15,0.62,0.07,-0.11,0.09,0.32,0.18,-0.34,0.27,0.18,0.32,0.77,0.8,-0.03,0.09,0.24,0.57,0.31,0.13,0.25,0.14,1.26,1.4,0.43,1.06,0.28,0.62,0.72,0.84,0.1,0.21,-0.28,0.13,0.26,0.9,0.63,0.19,0.04,0.33,0.48,0.25,0.1,0.36,0.11,0.05,0.75,0.76,0.42,0.19,0.38,0.2,0.66,0.96,0.93,0.79,-0.3,-0.29,0.84,2.58,0.87,-0.24,0.61,0.2,0.17,1.05)
-x=x*100
-y=y*100
-
-ynaive = x
->>>>>>> 88997c1 (third commit)
 
 gcv11<-function(x,y,a,b)
 {
   n=length(y)
+  print(n)
   gcv=10^10
+  aic=10^10
   t1=seq(min(x),max(x),length.out=100)
   p=rep(0,(n-2))
   for(z in 1:(n-2))
@@ -58,10 +51,7 @@ gcv11<-function(x,y,a,b)
       deter=det(wtw)
       z = MPL(wtw)
       beta= z %*% (t(w) %*% y)
-<<<<<<< HEAD
       S= w %*% z %*% t(w)
-=======
->>>>>>> 88997c1 (third commit)
       mu= w %*% beta
       MSE = t(y-mu) %*% (y-mu)/n
       I <- matrix(0, ncol = n, nrow = n)
@@ -70,17 +60,77 @@ gcv11<-function(x,y,a,b)
       k = sum(diag(I-S))
       AIc = (n+((n*log(2*pi))+n*(log(MSE))+(2*(1+m))))
       GCV = MSE / (k/n)^2
-      if(gcv>=AIc)
+      if(gcv>=GCV)
       {
-        gcv=AIc
+        gcv=GCV
+        knot1=k1[j1]
+        det=deter
+        orde1=m
+      }
+      if(aic>=AIc)
+      {
+        aic=AIc
         knot1=k1[j1]
         det=deter
         orde1=m
       }
     }
-    cat("orde = ", orde1, "knot 1 = ", knot1, "AIC = ", AIc, "GCV = ", GCV, "determinan = ", deter,"\n")
+    cat("orde = ", orde1, "knot 1 = ", knot1, "GCV = ", GCV, "AIC = ", AIc, "determinan = ", deter,"\n")
     
     gcv=10^10
+    aic=10^10
+  }
+}
+
+aic1<-function(x,y,a,b)
+{
+  n=length(y)
+  aic=10^10
+  t1=seq(min(x),max(x),length.out=100)
+  p=rep(0,(n-2))
+  for(z in 1:(n-2))
+  {
+    p[z]=t1[z+1]
+  }
+  comb1=combn(p,1,FUN = NULL)
+  c1=t(comb1)
+  for (m in 2:4)
+  {
+    k1=c1[,1]
+    v1=length(k1)
+    for (j1 in 1:v1) {
+      bs1=bs(x, df=NULL, knot=k1[j1], degre=m-1, intercept=TRUE, Boundary.knots=range(x))
+      w = cbind(bs1)
+      wtw = t(w) %*% w
+      deter=det(wtw)
+      z = MPL(wtw)
+      beta= z %*% (t(w) %*% y)
+      S= w %*% z %*% t(w)
+      mu= w %*% beta
+      MSE = t(y-mu) %*% (y-mu)/n
+      I <- matrix(0, ncol = n, nrow = n)
+      for (j in 1:n) 
+        I[j,j]=1
+      k = sum(diag(I-S))
+      AIc = (n+((n*log(2*pi))+n*(log(MSE))+(2*(1+m))))
+      if(gcv>=GCV)
+      {
+        gcv=GCV
+        knot1=k1[j1]
+        knot2=k2[j1]
+        orde1=m
+      }
+      if(aic>=AIc)
+      {
+        aic=AIc
+        knot1=k1[j1]
+        det=deter
+        orde1=m
+      }
+    }
+    cat("orde = ", orde1, "knot 1 = ", knot1, "AIC = ", AIc, "determinan = ", deter,"\n")
+    
+    aic=10^10
   }
 }
 
@@ -109,10 +159,7 @@ gcv22<-function(x,y,a,b)
       deter=det(wtw)
       z = MPL(wtw)
       beta= z %*% (t(w) %*% y)
-<<<<<<< HEAD
       S= w %*% z %*% t(w)
-=======
->>>>>>> 88997c1 (third commit)
       mu= w %*% beta
       MSE = t(y-mu) %*% (y-mu)/n
       I <- matrix(0, ncol = n, nrow = n)
@@ -139,6 +186,7 @@ gcv22<-function(x,y,a,b)
     cat("orde = ", orde1, "knot 1 = ", knot1, "knot 2 = ", knot2, "GCV = ", GCV, "AIC = ", AIc, "determinan = ", deter,"\n")
     
     gcv=10^10
+    aic=10^10
   }
 }
 
@@ -168,10 +216,7 @@ gcv33<-function(x,y,a,b)
       deter=det(wtw)
       z = MPL(wtw)
       beta= z %*% (t(w) %*% y)
-<<<<<<< HEAD
       S= w %*% z %*% t(w)
-=======
->>>>>>> 88997c1 (third commit)
       mu= w %*% beta
       MSE = t(y-mu) %*% (y-mu)/n
       I <- matrix(0, ncol = n, nrow = n)
@@ -200,6 +245,7 @@ gcv33<-function(x,y,a,b)
     cat("orde = ", orde1, "knot 1 = ", knot1, "knot 2 = ", knot2, "knot 3 = ", knot3, "AIC = ", AIc, "GCV = ", GCV, "determinan = ", deter,"\n")
     
     gcv=10^10
+    aic=10^10
   }
 }
 
@@ -230,10 +276,7 @@ gcv44<-function(x,y,a,b)
       deter=det(wtw)
       z = MPL(wtw)
       beta= z %*% (t(w) %*% y)
-<<<<<<< HEAD
       S= w %*% z %*% t(w)
-=======
->>>>>>> 88997c1 (third commit)
       mu= w %*% beta
       MSE = t(y-mu) %*% (y-mu)/n
       I <- matrix(0, ncol = n, nrow = n)
@@ -264,15 +307,17 @@ gcv44<-function(x,y,a,b)
     cat("orde = ", orde1, "knot 1 = ", knot1, "knot 2 = ", knot2, "knot 3 = ", knot3, "knot 4 = ", knot4, "AIC = ", AIc, "GCV = ", GCV, "determinan = ", deter,"\n")
     
     gcv=10^10
+    aic=10^10
   }
 }
 
 #estimasi parameter
 bspline<-function(x,y,m,k){
   n<-length(y)
+  print(n)
   knot<-c(k)
   knot<-as.matrix(knot)
-  k<-length(knot)
+  k1<-length(knot)
   bs1 = bs(x,df=NULL, knots=k, degree=m-1, intercept=TRUE, Boundary.knots=range(x))
   w = cbind(bs1)
   wtw = t(w) %*% w
@@ -285,17 +330,18 @@ bspline<-function(x,y,m,k){
   I <- matrix(0, ncol=n, nrow = n)
   for (j in 1:n) 
     I[j,j] = 1
-  k = sum(diag(I-S))
-  GCV=(MSE/(k/n)^2)
-  AIc = (n+((n*log(2*pi))+n*(log(MSE))+(2*(4+m))))
+  l = sum(diag(I-S))
+  GCV=(MSE/(l/n)^2)
+  AIc = (n+((n*log(2*pi))+n*(log(MSE))+(2*(3+m))))
   cat("NIlai GCV adalah ","\n",GCV,"\n")
   cat("NIlai AIC adalah ","\n",AIc,"\n")
 }
 #pengujian parameter
 Uji_Parameter<-function(x,y,m,k){
+  n<-length(y)
   knot<-c(k)
   knot<-as.matrix(knot)
-  k<-length(knot)
+  k1<-length(knot)
   orde = (m-1)
   bs1=bs(x,df=NULL, knots=k, degree=m-1, intercept=TRUE, Boundary.knots=range(x))
   w = cbind(bs1)
@@ -311,9 +357,9 @@ Uji_Parameter<-function(x,y,m,k){
   MSE = ((t(y-yhat)) %*% (y-yhat))/n
   MSE <- as.numeric(MSE)
   SSR <- sum((yhat-ybar)^2)
-  MSR <- SSR/((orde+k)-1)
+  MSR <- SSR/((orde+k1)-1)
   SSE <- sum((y-yhat)^2)
-  MSER <- SSE/(n-(orde+k))
+  MSER <- SSE/(n-(orde+k1))
   JKT = sum((y-ybar)^2)
   Fhit_1 = MSR/MSER
   cat("Nilai F hitung pengujian serentak adalah","\n", Fhit_1,"\n")
@@ -322,12 +368,13 @@ Uji_Parameter<-function(x,y,m,k){
   cat("Analysis Of Variance (ANOVA)","\n")
   cat("===================================================","\n")
   cat("Sumber df   SS    MS      Fhitung","\n")
-  cat("Regresi",((orde+k)-1), "",SSR,"",MSR,"",Fhit_1,"\n")
-  cat("Error", (n-(orde+k)), "", SSE, "", MSER, "", "\n")
+  cat("Regresi",((orde+k1)-1), "",SSR,"",MSR,"",Fhit_1,"\n")
+  cat("Error", (n-(orde+k1)), "", SSE, "", MSER, "", "\n")
   cat("Total ", (n-1), "", JKT, "", "\n")
+  print(MSE)
   cat("===================================================", "\n")
 }
-
+n
 
 #mencari knot dari orde 2 sampai 4 dan 1knot sampai 4knot
 gcv11(x,y,a,b)
@@ -336,20 +383,12 @@ gcv33(x,y,a,b)
 gcv44(x,y,a,b)
 
 #menentukan parameter dari titik knot optimal
-<<<<<<< HEAD
 bspline(x,y,3,k=c(-0.1327273,-0.1185859,-0.1044444))
 bspline(x,y,4,k=c(-0.07616162,0.5319192,0.5460606,0.560202))
 
 #uji parameter
 Uji_Parameter(x,y,3, k=c(-0.1327273,-0.1185859,-0.1044444))
 Uji_Parameter(x,y,4, k=c(-0.07616162,0.5319192,0.5460606,0.560202))
-=======
-bspline(x,y,2,k=c(1.252727))
-bspline(x,y,4,k=c(1.016768, 1.046263, 1.075758, 1.105253))
-
-#uji parameter
-Uji_Parameter(x,y,2, k=c(1.252727))
->>>>>>> 88997c1 (third commit)
 
 #f table
 qf(p = 0.05, df1=1, df2=70, lower.tail = FALSE)
--- uji parameter.R
+++ uji parameter.R
@@ -1,8 +1,9 @@
 #pengujian parameter
 Uji_Parameter<-function(x,y,m,k){
+  n<-length(y)
   knot<-c(k)
   knot<-as.matrix(knot)
-  k<-length(knot)
+  k1<-length(knot)
   orde = (m-1)
   bs1=bs(x,df=NULL, knots=k, degree=m-1, intercept=TRUE, Boundary.knots=range(x))
   w = cbind(bs1)
@@ -18,9 +19,9 @@ Uji_Parameter<-function(x,y,m,k){
   MSE = ((t(y-yhat)) %*% (y-yhat))/n
   MSE <- as.numeric(MSE)
   SSR <- sum((yhat-ybar)^2)
-  MSR <- SSR/((orde+k)-1)
+  MSR <- SSR/((orde+k1)-1)
   SSE <- sum((y-yhat)^2)
-  MSER <- SSE/(n-(orde+k))
+  MSER <- SSE/(n-(orde+k1))
   JKT = sum((y-ybar)^2)
   Fhit_1 = MSR/MSER
   cat("Nilai F hitung pengujian serentak adalah","\n", Fhit_1,"\n")
@@ -29,36 +30,17 @@ Uji_Parameter<-function(x,y,m,k){
   cat("Analysis Of Variance (ANOVA)","\n")
   cat("===================================================","\n")
   cat("Sumber df   SS    MS      Fhitung","\n")
-  cat("Regresi",((orde+k)-1), "",SSR,"",MSR,"",Fhit_1,"\n")
-  cat("Error", (n-(orde+k)), "", SSE, "", MSER, "", "\n")
+  cat("Regresi",((orde+k1)-1), "",SSR,"",MSR,"",Fhit_1,"\n")
+  cat("Error", (n-(orde+k1)), "", SSE, "", MSER, "", "\n")
   cat("Total ", (n-1), "", JKT, "", "\n")
+  print(MSE)
   cat("===================================================", "\n")
-  
-  #pengujian individu
-  cat("Uji Individu \n")
-  Sbett<-matrix(0,nrow = m+k+1, ncol = 1)
-  Sbett<-sqrt(diag(MSE*(t(w)%*%w)))
-  Sbett<-as.matrix(Sbett)
-  thitung<-bet/Sbett 
-  thitung
-  # SEbeta<-matrix(0, nrow = (orde+k), ncol = (orde+k))
-  # Ai<- solve(wtw)
-  # SEbeta <- sqrt(diag((MSE*Ai)))
-  # SEbeta <- as.matrix(SEbeta)
-  # Thit<- Beta/SEbeta
-  # print(Thit)
 }
 
 
-<<<<<<< HEAD
 #uji parameter
 Uji_Parameter(x,y,3, k=c(-0.1327273,-0.1185859,-0.1044444))
 Uji_Parameter(x,y,4, k=c(-0.07616162,0.5319192,0.5460606,0.560202))
-=======
-#uji model paramter
-Uji_Parameter(x,y,2,k=c(1.252727))
-Uji_Parameter(x,y,4,k=c(1.016768, 1.046263, 1.075758, 1.105253))  
->>>>>>> 88997c1 (third commit)
   
 
 #pengujian parameter 2
@@ -120,19 +102,15 @@ Uji_tahap2(x,y,2,k=c(1.252727))
 predik<-function(x,y,m,k){
   knot<-c(k)
   knot<-as.matrix(knot)
-  k<-length(knot)
+  k1<-length(knot)
   bs1=bs(x,df=NULL, knots=k, degree=m-1, intercept=TRUE, Boundary.knots=range(x))
-<<<<<<< HEAD
   w = cbind(bs1)
-=======
-  w = cbind(bs1[,2:3])
->>>>>>> 88997c1 (third commit)
   w = as.matrix(w)
   wtw = t(w) %*% w
   z = MPL(wtw)
   bet = (z %*% (t(w) %*% y))
   Beta = as.matrix(bet)
-  yhat = (w %*% bet)
+  yhat = (w %*% Beta)
   
   predik = cbind(y,yhat)
   predik_w = as.matrix(predik)
@@ -141,79 +119,33 @@ predik<-function(x,y,m,k){
   
 }
 
-<<<<<<< HEAD
 predik(x,y,3, k=c(-0.1327273,-0.1185859,-0.1044444))
 predik(x,y,4, k=c(-0.07616162,0.5319192,0.5460606,0.560202))
 
-
-
+install.packages("MLmetrics")
+library(MLmetrics)
 #mape
 yhat = predik(x,y,4, k=c(-0.07616162,0.5319192,0.5460606,0.560202))[,2]
-MAPE = mean(abs(y-yhat)/y)*100
-MAPE
+# MAPE = mean(abs((y-yhat)/y))*100
+# MAPE
+MAPE(yhat, y)
 plot(x,type = "o", col = "#1ECBE1")
 lines(yhat, type = "l" ,col = "#E11ECB")
 lines(yhat1, col = "#66710F")
 legend(1, 1.15, legend=c("Data Aktual", "Data Prediksi AIC", "Data Prediksi GCV"), cex=1,
        fill = c("#1ECBE1","#E11ECB","#66710F")
 )
-=======
-
-#uji terakhir
-predic <- function(x,y,m,k)
-{
-  knot<-c(k)
-  knot<-as.matrix(knot)
-  k<-length(knot)
-  n<-length(x)
-  bs1=bs(x,df=NULL, knots=k, degree=m-1, intercept=TRUE, Boundary.knots=range(x))
-  bs1n = bs1[,2:3]
-  wn = cbind(bs1n)
-  wn = as.matrix(wn)
-  
-  N <- matrix(0, ncol = n, nrow = n)
-  diag(N) = (1/x)
-  
-  A <- t(wn) %*% N %*% wn
-  A = as.matrix(A)
-  B <- t(wn) %*% N %*% y
-  Beta <- A %*% B
-  Beta_w <- as.matrix(Beta)
-  yhat_w <- wn %*% Beta_w
-  
-  pred = cbind(y,yhat_w)
-  pred = as.matrix(pred)
-  cat("Nilai prediksi adalah \n" )
-  print(pred)
-}
-
-predic(x,y,2,k=c(1.252727))
-
-#mape
-yhat = predik(x,y,2,k = c(1.252727))[,2]
-MAPE = mean(abs(y-yhat)/y)*100
-MAPE
-plot(y,type = "l", col = "red")
-lines(yhat, col = "blue")
->>>>>>> 88997c1 (third commit)
 # plot(yhat,y)
 # plot(sapply(y, function(x) min(150, x)))
 
 #mape
 #mape
-<<<<<<< HEAD
 yhat1 = predik(x,y,3, k=c(-0.1327273,-0.1185859,-0.1044444))[,2]
-MAPE = mean(abs(y-yhat1)/y)*100
-MAPE
+# MAPE = mean(abs((y-yhat1)/y))*100
+# MAPE
+MAPE(yhat1, y)
 plot(x,type = "l", col = "red")
 lines(yhat1, col = "#CBE11E")
 legend(2, 1, legend=c("Data Aktual", "Data Prediksi"), cex=0.5,
        fill = c("red","purple")
 )
-=======
-yhat1 = predik(x,y,4,k = c(1.016768, 1.046263, 1.075758, 1.105253))[,2]
-MAPE = mean(abs(y-yhat)/y)*100
-MAPE
-plot(y,type = "l", col = "red")
-lines(yhat, col = "blue")
->>>>>>> 88997c1 (third commit)
